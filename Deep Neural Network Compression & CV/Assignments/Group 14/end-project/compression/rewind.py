c506472c32b8d2b146020ee8e870b112a4c6c310
ponishbhatia <ponishbhatia@gmail.com>
2026-05-10 05:31:12 +0530

    Rename folder

--- Deep Neural Network Compression & CV/Assignments/Group 14/end-project/compression/rewind.py
+++
@@ -1,7 +0,0 @@
-import torch
-
-def rewind_weights(model, initial_state_dict, masks):
-    with torch.no_grad():
-        for name, param in model.named_parameters():
-            if 'weight' in name:
-                param.copy_(initial_state_dict[name] * masks[name])
--- Deep Neural Network Compression & CV/Assignments/Group 14/end-project/deploy.py
+++
@@ -1,4 +0,0 @@
-from deployment.load_model import load_compressed_model, run_inference
-
-model = load_compressed_model("compressed_models/vgg_compressed.npz")
-run_inference(model)
